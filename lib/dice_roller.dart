fdd11d260dddb090da9233cc6c712927fc41a68e
shunphoenix55 <76583147+shunphoenix55@users.noreply.github.com>
2023-06-19 17:55:11 +0530

    feat: Add Randomizer

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
   @override
   State<DiceRoller> createState() {
     return _DiceRollerState();
@@ -8,11 +13,11 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-2.png';
+  var currentDiceRoll = 2;
 
   void rollDice() {
     setState(() {
-      activeDiceImage = 'assets/images/dice-4.png';
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -22,7 +27,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
         TextButton(
